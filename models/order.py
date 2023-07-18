896eaa3c65ebd7c90177ec9d7662a4f9fc8a9bc6
Brandon <Brandonrw97@gmail.com>
2023-07-18 20:31:40 +0100

    MVP done on flask_lab will be trying to do more

---
+++ models/order.py
@@ -0,0 +1,8 @@
+class Order():
+
+    def __init__(self, customer_name, order_date, quanitity, game_title, description):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quanitity = quanitity
+        self.game_title = game_title
+        self.description = description
---
+++ models/order_list.py
@@ -0,0 +1,7 @@
+from models.order import Order
+
+order_1 = Order("Holly", "13/02/23", 2, "breath of the wild", "a zelda game on switch")
+order_2 = Order("Tammy", "31/05/23", 1, "FF14", "One of the morst addictive games going")
+order_3 = Order("Alex", "28/12/22", 1, "PUBG", "Clunky but Funky")
+
+orders = [order_1, order_2, order_3]
